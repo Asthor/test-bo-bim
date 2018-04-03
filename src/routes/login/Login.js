b8e6ca611a5461e27492b1cc22573503e91394f3
romain.guerin <romain.guerin@getbam.com>
2018-04-03 16:20:18 +0200

    Initial

---
+++ src/routes/login/Login.js
@@ -0,0 +1,66 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './Login.css';
+import { Button } from "@blueprintjs/core";
+
+class Login extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+  };
+
+  render() {
+    return (
+      <div className={s.root}>
+        <div className={s.container}>
+          <h1>{this.props.title}</h1>
+          <p className={s.lead}>
+            Identifiez-vous avec votre identifiant et mot de passe.
+          </p>
+          <form method="post">
+            <div className={s.formGroup}>
+              <label className={s.label} htmlFor="usernameOrEmail">
+                Adresse email:
+                <input
+                  className={s.input}
+                  id="usernameOrEmail"
+                  type="text"
+                  name="usernameOrEmail"
+                  autoFocus // eslint-disable-line jsx-a11y/no-autofocus
+                />
+              </label>
+            </div>
+            <div className={s.formGroup}>
+              <label className={s.label} htmlFor="password">
+                Mot de passe:
+                <input
+                  className={s.input}
+                  id="password"
+                  type="password"
+                  name="password"
+                />
+              </label>
+            </div>
+            <div className={s.formGroup}>
+              <button className={s.button} type="submit">
+                Connexion
+              </button>
+            </div>
+          </form>
+          <Button type="button" class="pt-button pt-icon-add">Default button</Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withStyles(s)(Login);
